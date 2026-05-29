ee680cf40ff7cfe63be6b664c1ee71e74b79e858
Barrett <bigbthebuilder@gmail.com>
2026-05-28 22:18:57 -0700

    Prepare collapsible document editor for Vercel

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'collapsible-docs-v1';
+const CACHE_NAME = 'collapsible-docs-v10';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest'];
 
 self.addEventListener('install', event => {
